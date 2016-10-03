dff6b38ce5ab6b828ba02e06ee7f903b97eebaf8
wlacampb <wac@poi.ng>
2016-10-03 08:12:44 -0400

    Found the solution . . . turns out the obvious answer was a function . . . *sigh*

--- 0.32 - Average List/Average List.py
+++ 0.32 - Average List/Average List.py
@@ -11,3 +11,21 @@ average_list = sum_list / counter
 print average_list
 
 # prints average of list
+
+b = [1, 2, 5, 10, 255, 3]
+
+def average_list(arr): 
+      sum_list = 0
+      counter = 0
+      
+      for items in arr:
+            sum_list += items
+            counter += 1
+
+      average_list = sum_list / counter
+
+      print average_list
+
+average_list([1, 2, 5, 10, 255, 3])
+
+# finds average of list without global declarations
