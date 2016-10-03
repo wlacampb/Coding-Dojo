dd7192b272d46c3f0219d8355046d75ba13b9e02
wlacampb <wac@poi.ng>
2016-10-03 07:38:53 -0400

    After CS50, well, that was way too easy . . . oh well

---
+++ 0.30 - Multiples/multiples.py
@@ -0,0 +1,13 @@
+counter_p1 = 1
+while counter_p1 < 1001:
+      print counter_p1
+      counter_p1 += 2
+
+# prints odd numbers from 1 to 1000
+
+counter_p2 = 5
+while counter_p2 < 1000001: 
+      print counter_p2
+      counter_p2 += 5
+
+# prints multiples of 5 from 5 to 10^6
