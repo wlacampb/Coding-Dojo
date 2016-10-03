a64ee4b0ad4a09ece804d9103f26746aca33d8bd
wlacampb <wac@poi.ng>
2016-10-03 08:06:45 -0400

    Reused code from Sum List . . . wondering how to implement a local counter and if doing so is actually worth it

---
+++ 0.32 - Average List/Average List.py
@@ -0,0 +1,13 @@
+a = [1, 2, 5, 10, 255, 3]
+sum_list = 0
+counter = 0
+
+for items in a:
+      sum_list += items
+      counter += 1
+
+average_list = sum_list / counter
+
+print average_list
+
+# prints average of list
