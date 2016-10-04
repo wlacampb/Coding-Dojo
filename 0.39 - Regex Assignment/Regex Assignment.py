4dba0e32c76999d3aaafdf0343037f7f28a2224e
Will Campbell <wac@poi.ng>
2016-10-04 10:50:40 -0400

    What have I learned? I hate weak typing
    It killed me in Stars and it's killed me again with this regex assignment.
    
    I can definitely say, though, half the battle learning to program is, first, getting in the habit of unit testing everything and, second, learning how to articulate your problem well enough when you Google it it actually leads you to exactly what you need.

--- 0.39 - Regex Assignment/Regex Assignment.py
+++ 0.39 - Regex Assignment/Regex Assignment.py
@@ -4,3 +4,15 @@ def get_matching_words(regex):
  words = ["aimlessness", "assassin", "baby", "beekeeper", "belladonna", "cannonball", "crybaby", "denver", "embraceable", "facetious", "flashbulb", "gaslight", "hobgoblin", "iconoclast", "issue", "kebab", "kilo", "laundered", "mattress", "millennia", "natural", "obsessive", "paranoia", "queen", "rabble", "reabsorb", "sacrilegious", "schoolroom", "tabby", "tabloid", "unbearable", "union", "videotape"]
 
  return [word for word in words if re.search(regex, word)]
+
+print ""
+get_matching_words(r"v") # "r" ensures the regex is not evaluted as a Python expression
+get_matching_words(r"ss")
+get_matching_words(r".*e$")
+get_matching_words(r"b\wb")
+get_matching_words(r"b\w+")
+get_matching_words(r"b\w*b")
+get_matching_words(r"\w*a\w*e\w*i\w*o\w*u")
+get_matching_words(r"^[regularexpsion]*$") # "^...$" evaluates from the beginning of the str, parses the first char for the specified chars "regularexpsion," parses the rest of the characters as such, and only prints the item from the arr if every character is a specific char
+get_matching_words(r"(.)\1")
+print ""
