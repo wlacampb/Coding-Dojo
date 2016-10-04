e06c905dc87e5b6f9b4baa6ac3147e48f06bb027
wlacampb <wac@poi.ng>
2016-10-04 12:12:59 -0400

    adds enumerated list

--- 0.40 - Bubble Sort/Bubble Sort.py
+++ 0.40 - Bubble Sort/Bubble Sort.py
@@ -1,1 +1,18 @@
-###initial commit###
+def bubble_sort():
+      print ""
+      arr = [1, 4, 2, 5]
+
+      print "ARR"
+      print arr
+      print ""
+
+      print "ENUMERATE"
+      new_arr = list(enumerate(arr))
+      print new_arr
+      print ""
+      
+      for index, item in new_arr:
+            print item
+      print ""
+
+bubble_sort()
