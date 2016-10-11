e690558a1f1336b42e3b7eb5b20677d7d41d23c4
wlacampb <wac@poi.ng>
2016-10-11 12:05:02 -0400

    changes

--- 0.40 - Bubble Sort/Bubble Sort.py
+++ 0.40 - Bubble Sort/Bubble Sort.py
@@ -1,18 +1,22 @@
-def bubble_sort():
+def bubbleSort(arr):
       print ""
-      arr = [1, 4, 2, 5]
-
-      print "ARR"
+      print "UNSORTED LIST"
       print arr
       print ""
 
-      print "ENUMERATE"
-      new_arr = list(enumerate(arr))
-      print new_arr
-      print ""
-      
-      for index, item in new_arr:
-            print item
+      # RANGE is a GENERATOR, which means it GENERATES a NEW LIST
+      for passthrough in range(len(arr) - 1):  # iterates i from 0 to 3 via a NEW LIST || range(len(arr)) ensures the algorithm passes through enough times, i.e. i, to sort the list, which you can confirm if you want manually by, well, executing the code in your head to find out the time complexity of the sort is at worst, which are current arr is, n^2 and with the iterator iterating through another iterator (0 - 4, 3, 2, 1; 1 - 4, 3, 2, 1; 2 - 4, 3, 2, 1; and so on), this constitutes n^2 comparisons
+            for index in range(len(arr) - 1, 0, -1):  # iterates k from 4 to 1 via a NEW LIST || len(arr) - 1 ensures that since the list ends at 1 [index - 1] in the subsequent lines compares to the [0]-th index instead of the [-1]-th index, which is out-of-range and thus -- presumably -- would make everything grind to a halt and catch fire
+                  if (arr[index] < arr[index - 1]):
+                        swap(arr, index, index - 1)
+
+      print "SORTED LIST"
+      print arr
       print ""
 
-bubble_sort()
+def swap(arr, x, y):
+      tmp = arr[x]
+      arr[x] = arr[y]
+      arr[y] = tmp
+
+bubbleSort([5,4,3,2,1])
